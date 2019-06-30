e6c9a020964a80de2877e3dfdae63d96f64ed99c
kyuuki <kyuuki.japan@gmail.com>
2019-06-30 12:06:31 +0900

    4 桁までの変換に対応

--- app/src/main/java/jp/kyuuki/kanjichanger/components/KanjiChanger.kt
+++ app/src/main/java/jp/kyuuki/kanjichanger/components/KanjiChanger.kt
@@ -1,40 +1,68 @@
 package jp.kyuuki.kanjichanger.components
 
 /**
- * 数字を漢字に変換
+ * 数字を漢字に変換.
+ *
+ * - 56781234 → 五千六百七十八万千弐百参拾四
+ */
+fun numberToKanji(number: Int): String {
+    if (number >= 10000) return "5 桁以上の数字にはまだ未対応です。"
+
+    return number4DigitToKanji(number)
+}
+
+/**
+ * 4 桁の数字を漢字に変換
  *
  * - 1234 → 千弐百参拾四
  */
-fun numberToKanji(number: Int): String {
-    if (number >= 100) return "3 桁以上の数字にはまだ未対応です。"
-
-    val x = number / 10  // 10 の位
-    val y = number % 10  // 1 の位
-
-    var str = ""   // 変換結果を入れる変数
-
-    // 10 の位も 1 の位も 0 の時は無条件で "零" に
-    if (x == 0 && y == 0) return "零"
-
-    // 10 の位を漢字に
-    if (x == 0) {
-        // 10 の位が 0 のときは何もつけない
-    } else if (x == 1) {
-        // 10 の位が 1 のときは "壱拾" のように "壱" はつけない
-        str += "拾"
-    } else {
-        str += number1DigitToKanji(x) + "拾"
+fun number4DigitToKanji(number: Int): String {
+    // 0 の時だけ特別
+    if (number == 0) {
+        return number1DigitToKanji(0)
     }
 
-    // 1 の位を漢字に
-    if (y == 0) {
-        // 1 の位が 0 のときは何もつけない
-    } else {
-        str += number1DigitToKanji(y)
+    var str = ""   // 変換結果を入れる変数
+    var x : Int  // 大きな位から一文字ずつ数字を取り出して入れる
+    var y = number  // 取り出した残り
+
+    // 千の位
+    x = y / 1000  // 千の位
+    y = y % 1000  // 残り
+
+    if (x != 0) {
+        str += number1DigitToKanji(x) + "千"
+    }
+
+    // 百の位
+    x = y / 100  // 百の位
+    y = y % 100  // 残り
+
+    if (x != 0) {
+        str += number1DigitToKanji(x) + "百"
+    }
+
+    // 十の位
+    x = y / 10  // 十の位
+    y = y % 10  // 残り (一の位)
+
+    if (x != 0) {
+        if (x == 1) {
+            // 十の位が 1 のときは "壱拾" のように "壱" はつけない
+            str += "拾"
+        } else {
+            str += number1DigitToKanji(x) + "拾"
+        }
+    }
+
+    // 一の位
+    if (y != 0) {
+      str += number1DigitToKanji(y)
     }
 
     return str
 }
+
 
 /**
  * 1 桁の数字を漢字に変換
@@ -42,6 +70,8 @@ fun numberToKanji(number: Int): String {
  * - 0 → 零, 1 → 壱, ... 9 → 九
  */
 fun number1DigitToKanji(number: Int): String {
+    // TODO: 0 ～ 9 以外が入力されたとき
+    // TODO: 定数がよさげ
     val kanjis = arrayListOf("零", "壱", "弐", "参", "四", "五", "六", "七", "八", "九")
     return kanjis[number]
 }
