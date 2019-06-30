685ce90fb1cd2bec49df4d8922e0e6fe19503501
kyuuki <kyuuki.japan@gmail.com>
2019-06-30 13:00:27 +0900

    4 桁より大きい数字の変換に対応

--- app/src/main/java/jp/kyuuki/kanjichanger/components/KanjiChanger.kt
+++ app/src/main/java/jp/kyuuki/kanjichanger/components/KanjiChanger.kt
@@ -4,17 +4,47 @@ package jp.kyuuki.kanjichanger.components
  * 数字を漢字に変換.
  *
  * - 56781234 → 五千六百七十八万千弐百参拾四
+ * TODO: 垓以上に対応したい
  */
-fun numberToKanji(number: Int): String {
-    if (number >= 10000) return "5 桁以上の数字にはまだ未対応です。"
+fun numberToKanji(number: Long): String {
+    // 0 の時だけ特別
+    if (number == 0L) {
+        return number1DigitToKanji(0)
+    }
 
-    return number4DigitToKanji(number)
+    // 桁の低い方から高い方へ
+    // https://ja.wikipedia.org/wiki/%E5%91%BD%E6%95%B0%E6%B3%95#%E5%A4%A7%E6%95%B0
+    // TODO: 定数がよさげ
+    val tanis = arrayListOf("", "万", "億", "兆", "京",
+        "垓", "\uD855\uDF71"/* 𥝱 */, "穣", "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由他", "不可思議", "無量大数")
+
+    var str = ""    // 変換結果を入れる変数
+    var x : Int     // 小さな位から四文字ずつ数字を取り出して入れる (1234567890 → 7890)
+    var y = number  // 取り出した残り (1234567890 → 123456)
+
+    tanis.forEach {
+        if (y == 0L) {
+            // https://improve-future.com/kotlin-why-while-break-or-continue-cant-be-used-in-foreacn-repeat-which-can-be-used-in-while-and-for.html
+            return@forEach
+        }
+
+        x = (y % 10000).toInt()
+        y = y / 10000
+
+        if (x != 0) {
+            val kanji = number4DigitToKanji(x)
+            str = "$kanji$it$str"
+        }
+    }
+
+    return str
 }
 
 /**
  * 4 桁の数字を漢字に変換
  *
  * - 1234 → 千弐百参拾四
+ * - 0 → 零
  */
 fun number4DigitToKanji(number: Int): String {
     // 0 の時だけ特別
@@ -22,8 +52,8 @@ fun number4DigitToKanji(number: Int): String {
         return number1DigitToKanji(0)
     }
 
-    var str = ""   // 変換結果を入れる変数
-    var x : Int  // 大きな位から一文字ずつ数字を取り出して入れる
+    var str = ""    // 変換結果を入れる変数
+    var x : Int     // 大きな位から一文字ずつ数字を取り出して入れる
     var y = number  // 取り出した残り
 
     // 千の位
