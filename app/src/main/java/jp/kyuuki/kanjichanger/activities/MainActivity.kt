ad41ba7674d847dc0e0fabae806244f1a9e04ebe
kyuuki <kyuuki.japan@gmail.com>
2019-07-07 14:38:08 +0900

    クリアボタン実装

--- app/src/main/java/jp/kyuuki/kanjichanger/activities/MainActivity.kt
+++ app/src/main/java/jp/kyuuki/kanjichanger/activities/MainActivity.kt
@@ -8,7 +8,6 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import jp.kyuuki.kanjichanger.R
 import jp.kyuuki.kanjichanger.models.KanjiChangeableNumber
 
@@ -20,13 +19,20 @@ class MainActivity : AppCompatActivity() {
 
         val editTextNumber = findViewById<EditText>(R.id.edit_text_number)
         val textResultKanji = findViewById<TextView>(R.id.text_result_kanji)
-        val buttonChange = findViewById<Button>(R.id.button_change)
+        val buttonConvert = findViewById<Button>(R.id.button_convert)
+        val buttonClear = findViewById<Button>(R.id.button_clear)
 
-        buttonChange.setOnClickListener {
+        buttonConvert.setOnClickListener {
             // TODO: 数字以外が入力された場合の対応
             val number = KanjiChangeableNumber(editTextNumber.text.toString().toLong())
             textResultKanji.text = number.getKanji()
         }
+
+        buttonClear.setOnClickListener {
+            editTextNumber.text = null
+            textResultKanji.text = null
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
